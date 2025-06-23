e1df2ecb27f9967d22cf14195d034d6c6c0da42b
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2025-06-23 13:49:56 -0400

    bump version to 0.5.3 in flake.nix and meson.build

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
           default = self.packages.${system}.gorg;
           gorg = pkgs.stdenv.mkDerivation {
             pname = "gorg";
-            version = "0.5.2";
+            version = "0.5.3";
 
             src = ./.;
 
